e559db46c68235e99b8bdf80dc6ebfe8af580c04
arifkhan676 <arifulhasan5500@gmail.com>
2023-08-24 00:10:23 +0600

    cart useEffect

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,53 +1,68 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import iPhone from '../../DeviceData/iPhone'
 import Products from '../Products/Products'
 import './Shop.css'
 import Cart from '../Cart/Cart'
-import { addToCartDatabase } from '../utilities/databaseManager'
+import { addToCartDatabase, getDatabaseCart } from '../utilities/databaseManager'
 
 const Shop = () => {
 
     const [products, setProducts] = useState(iPhone)
 
     const [cartBtn, setcartBtn] = useState([]);
+
+
+    useEffect(() => {
+        const saveCart = getDatabaseCart()
+        const productId = Object.keys(saveCart);
+
+        const CartList = productId.map(mainId => {
+            const product = iPhone.find(pd => pd.id === mainId)
+            product.quantity = saveCart[mainId];
+            return product;
+        })
+        setProducts(CartList);
+
+    }, [])
     // console.log(cartBtn);
-    const btnClick = (item) => {
-        const sameProduct = cartBtn.find(pd => pd.id === item.id);
-        let count = 1;
-        let newCart;
-        if (sameProduct) {
-            count = sameProduct.quantity + 1;
-            sameProduct.quantity = count;
-            const others = cartBtn.filter(pd => pd.id !== item.id);
-            newCart = [...others, sameProduct]
-        } else {
-            item.quantity = 1;
-            newCart = [...cartBtn, item]
-        }
-        setcartBtn(newCart)
-        addToCartDatabase(item.id, count)
+    const btnClick = (it em) => {
+    const sameProduct = cartBtn.find(pd => pd.id === item.id);
+    let count = 1;
+    let newCart;
+    if (sameProduct) {
+        count = sameProduct.quantity + 1;
+        sameProduct.quantity = count;
+        const others = cartBtn.filter(pd => pd.id !== item.id);
+        newCart = [...others, sameProduct]
+    } else {
+        item.quantity = 1;
+        newCart = [...cartBtn, item]
     }
+    setcartBtn(newCart)
+    addToCartDatabase(item.id, count)
+}
 
-    return (
-        <div className='shop-container' >
-            <div className="products-container">
 
-                {products.map((product) =>
-                    <Products
-                        key={product.id}
-                        item={product}
-                        handleClick={btnClick}
-                        showAddCart={true}
-                    />)}
+return (
+    <div className='shop-container' >
+        <div className="products-container">
 
-            </div>
-            <div className="cart-container">
-                <Cart Cart={cartBtn}
-                />
-            </div>
+            {products.map((product) =>
+                <Products
+                    key={product.id}
+                    item={product}
+                    handleClick={btnClick}
+                    showAddCart={true}
+                />)}
 
         </div>
-    )
+        <div className="cart-container">
+            <Cart Cart={cartBtn}
+            />
+        </div>
+
+    </div>
+)
 }
 
 export default Shop
